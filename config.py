8d777661284431f56c3e426ce6d57cc4e8ba90f9
kafuuchino123 <chinochan233@163.com>
2025-11-07 20:37:36 +0800

    修改为用api生成语言，修改ingest.py以更好分割pdf

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ MODE = "api"
 API_PROVIDER = "moonshot"
 
 # 填入你的 API Key
-MOONSHOT_API_KEY = "sk-2C74H01I3lzME5VsFtio0Vx7mc3PGpso45x3EoSRAn324Z68"  # 若选择Moonshot
+MOONSHOT_API_KEY = "你的api"  # 若选择Moonshot
